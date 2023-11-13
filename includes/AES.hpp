f364a80abe52291bf9f9a28c36caa36e90bb1ee9
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-11-13 13:09:01 +0400

    [ADD] Added instance variables and constructor declaration

--- includes/AES.hpp
+++ includes/AES.hpp
@@ -4,9 +4,26 @@
 #include "sbox.hpp"
 #include "gfmul.hpp"
 
+enum class AESKeyLen 
+{ 
+    AES_128, 
+    AES_192, 
+    AES_256 
+};
+
 class AES 
 {
+    private:
+        static const unsigned int   NUM_COL = 4;
+        static const unsigned int   BLOCK_SIZE = 4 * NUM_COL * sizeof(unsigned char);
 
+        unsigned int    num_rounds;
+        unsigned int    num_words; // In Key
+
+    public:
+        explicit AES(const AESKeyLen key_len = AESKeyLen::AES_256);
+
+        // TODO: complete the encrypt/decrypt function declarations
 };
 
 #endif
