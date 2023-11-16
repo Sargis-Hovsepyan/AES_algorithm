79dd6ad2ba8bd77fcd9213ceff74bdb7b8b90f3b
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-11-16 15:06:44 +0400

    [ADD] Added simple AES tests

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,26 +1,10 @@
 #include "AES.hpp"
-#include <iostream>
+#include "aes_test.hpp"
 
-const unsigned int BLOCK_LENGTH = 16 * sizeof(unsigned char);
 
 int main()
 {
-    AES aes(AESKeyLen::AES_128);
-
-    AES::byte   plain[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
-    AES::byte   key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-    
-    AES::byte   *cipher = aes.encrypt(plain, key, BLOCK_LENGTH);
-    AES::byte   *decrypted = aes.decrypt(cipher, key, BLOCK_LENGTH);
-
-    std::cout << "plaintex HEX: "; 
-    aes.print_hex(plain, BLOCK_LENGTH);
-    
-    std::cout << "ciphertext HEX: ";
-    aes.print_hex(cipher, BLOCK_LENGTH);
-    
-    std::cout << "decrypted HEX: ";
-    aes.print_hex(decrypted, BLOCK_LENGTH);
-
+    std::cout << "\t\tSIMPLE TESTS FOR AES ALGORITHM\n" << std::endl;
+    run_all_tests();
     return 0;
 }
