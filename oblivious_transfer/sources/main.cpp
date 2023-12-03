59103ff3d5593be37421fa3ceb4e422d934dd267
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-12-03 16:40:00 +0400

    [ADD] Added small test program

--- oblivious_transfer/sources/main.cpp
+++ oblivious_transfer/sources/main.cpp
@@ -1,25 +1,56 @@
 #include <iostream>
-#include "rsa.hpp"
+#include "Receiver.hpp"
+
+void    print(vector<size_t> v)
+{
+    for (size_t c : v)
+        std::cout << static_cast<char>(c);
+    std::cout << std::endl;
+}
 
 int main()
 {
-    rsa rsa_alg(47, 53);
+    RSA         rsa(43, 47);
 
-    string message = "Hello RSA!";
-    std::cout << "Original Message: " << message << std::endl;
+    string      message_0 = "First Message!";
+    string      message_1 = "Second Message!";
+    int         choice = 0;
 
-    // Encryption
-    vector<size_t> ciphertext = rsa_alg.encrypt(message);
+    /* Presenting the messages and the choice for OT */
 
-    std::cout << "Encrypted Message: ";
-    for (size_t c : ciphertext)
-         std::cout << std::to_string(c);
-    std::cout << std::endl;
-    
+    std::cout << "The first message: " << message_0 << std::endl;
+    std::cout << "The second message: " << message_1 << std::endl;
+    std::cout << "The original choice: " << choice << std::endl;;
 
-    // Decryption
-    string decrypted_message = rsa_alg.decrypt(ciphertext);
-    std::cout << "Decrypted Message: " << decrypted_message << std::endl;
+    Sender      sender(message_0, message_1, rsa);
+    Receiver    receiver(choice, sender);
+
+    /* Showing the values of x and v from OT */
+
+    Sender::pair    x = sender.get_random_messages();
+    vector<size_t>  v = receiver.blind_encrypt(x[0], x[1]);
+
+
+    std::cout << "\n\n";
+    std::cout << "The value sent by the sender x_0: ";
+    print(x[0]);
+    std::cout << "The value sent by the sender x_1: ";
+    print(x[1]);
+    std::cout << "The value sent by the receiver v: ";
+    print(v);
+
+    /* Showing messages sent and chosen from OT */
+
+    Sender::pair    messages = sender.send(v);
+    vector<size_t>  chosen = receiver.receive(messages[0], messages[1]);
+
+    std::cout << "\n\n";
+    std::cout << "The message sent by the sender m_0: ";
+    print(messages[0]);
+    std::cout << "The message sent by the sender m_1: ";
+    print(messages[1]);
+    std::cout << "The message chosen by the receiver: ";
+    print(chosen);
 
     return 0;
 }
