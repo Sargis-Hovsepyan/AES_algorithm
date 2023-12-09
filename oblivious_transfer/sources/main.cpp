396408254fdaa8341bcec7eabdb38769373afdea
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2023-12-09 21:37:28 +0400

    [ADD] Simulation of OT

--- oblivious_transfer/sources/main.cpp
+++ oblivious_transfer/sources/main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include "Receiver.hpp"
 
-void    print(vector<size_t> v)
+void    print(vector<long long> v)
 {
-    for (size_t c : v)
-        std::cout << static_cast<char>(c);
+    for (long long c : v)
+        std::cout << c << " ";
     std::cout << std::endl;
 }
 
 int main()
 {
-    RSA         rsa(43, 47);
-
+    RSA         rsa(67, 83);
+    
     string      message_0 = "First Message!";
     string      message_1 = "Second Message!";
-    int         choice = 0;
+    int         choice = 1;
 
     /* Presenting the messages and the choice for OT */
 
@@ -28,29 +28,27 @@ int main()
     /* Showing the values of x and v from OT */
 
     Sender::pair    x = sender.get_random_messages();
-    vector<size_t>  v = receiver.blind_encrypt(x[0], x[1]);
+    vector<long long>  v = receiver.blind_encrypt(x[0], x[1]);
 
 
     std::cout << "\n\n";
-    std::cout << "The value sent by the sender x_0: ";
-    print(x[0]);
-    std::cout << "The value sent by the sender x_1: ";
-    print(x[1]);
-    std::cout << "The value sent by the receiver v: ";
-    print(v);
+    std::cout << "The value sent by the sender x_0: "; print(x[0]);
+    std::cout << "The value sent by the sender x_1: "; print(x[1]);
+    std::cout << "The value sent by the receiver v: "; print(v);
 
     /* Showing messages sent and chosen from OT */
 
     Sender::pair    messages = sender.send(v);
-    vector<size_t>  chosen = receiver.receive(messages[0], messages[1]);
+    vector<long long>  chosen = receiver.receive(messages[0], messages[1]);
 
     std::cout << "\n\n";
-    std::cout << "The message sent by the sender m_0: ";
-    print(messages[0]);
-    std::cout << "The message sent by the sender m_1: ";
-    print(messages[1]);
+    std::cout << "The message sent by the sender m_0: "; print(messages[0]);
+    std::cout << "The message sent by the sender m_1: "; print(messages[1]);
     std::cout << "The message chosen by the receiver: ";
-    print(chosen);
+
+    for (long long c : chosen)
+        std::cout << static_cast<char>(c);
+    std::cout << std::endl;
 
     return 0;
 }
